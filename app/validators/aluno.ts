f09fdec9c7e6d33b19db17ef37217a68e8e3e460
Genesio Antonio <2018genesio@gmail.com>
2024-05-30 12:34:37 +0100

    mensagens personalizadas dos validator

--- app/validators/aluno.ts
+++ app/validators/aluno.ts
@@ -1,25 +1,26 @@
 /* eslint-disable prettier/prettier */
 
-import vine from "@vinejs/vine"
+import vine, { SimpleMessagesProvider }  from "@vinejs/vine"
 
 export const createAlunoValidator = vine.compile(
   vine.object({
     nome: vine.string().trim().escape().maxLength(100).minLength(2),
     idade: vine.number().positive().withoutDecimals().range([5,200]),
     periodo: vine.string().trim().escape().maxLength(100).minLength(2),
-    estado: vine.number(),
+    estado: vine.number().range([0,1]),
   }),
 )
 
-export const messeges = {
-  string: '{{ field }} deve ser uma string',
-  number: '{{ field }} deve ser um número',
-  minLength: '{{ field }} '
-}
-
-//const messeges = {
-  //string: '{{ field }} deve'
-//}
+vine.messagesProvider = new SimpleMessagesProvider({
+  // Applicable for all fields
+  'required': 'O campo {{ field }} é obrigatório',
+  'string': 'O campo {{ field }} deve ser do tipo string',
+  'escape': 'Caracteres invalidos',
+  'maxLength' : 'O campo {{field}} permite no maximo 100 caracteres',
+  'minLength' : 'O campo {{field}} deve conter minimo 2 caracteres',
+  // Error message for the username field
+  //'username.required': 'Please choose a username for your account',
+})
 
 
 export const updateAlunoValidator = vine.compile(
@@ -27,6 +28,6 @@ export const updateAlunoValidator = vine.compile(
         nome: vine.string().trim().escape().maxLength(100).minLength(2),
         idade: vine.number().positive().withoutDecimals().range([5,200]),
         periodo: vine.string().trim().escape().maxLength(100).minLength(2),
-        estado: vine.number(),
+        estado: vine.number().range([0,1]),
       })
 )
