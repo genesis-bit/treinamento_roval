d2f55cc1c7f1509277f16b8417ee97208c6f732d
Genesio Antonio <2018genesio@gmail.com>
2024-06-03 11:57:55 +0100

    add model ano lectivo

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,9 @@
 |
 */
 
-import AlunosController from '#controllers/alunos_controller'
-import ProfessorsController from '#controllers/professors_controller'
-import TurmasController from '#controllers/turmas_controller'
+// import AlunosController from '#controllers/alunos_controller'
+// import ProfessorsController from '#controllers/professors_controller'
+// import TurmasController from '#controllers/turmas_controller'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
@@ -19,10 +19,10 @@ router.get('/', async () => {
   }
 })
 
-router.resource('alunos', AlunosController)
-router.resource('turma-alunos', TurmasController)
-router.resource('turmas', TurmasController)
-router.resource('professores', ProfessorsController)
+// router.resource('alunos', AlunosController)
+// router.resource('turma-alunos', TurmasController)
+// router.resource('turmas', TurmasController)
+// router.resource('professores', ProfessorsController)
 
 //router.get('alunos', [AlunosController, 'index']).as('aluno.index')
 //router.post('alunos', [AlunosController, 'store']).as('aluno.store')
