aeb7f23838c2f9c32b21310993610c0b7b4e0c6f
Genesio Antonio <2018genesio@gmail.com>
2024-06-03 12:11:30 +0100

    add controller, model e migration de sala e aluno

---
+++ database/migrations/1717412825433_create_alunos_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'alunos'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('nome', 50).notNullable()
+      table.string('genero', 10).notNullable()
+      table.date('data_nascimento').notNullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
